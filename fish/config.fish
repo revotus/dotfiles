72b682d2afe05b94531e07dd7971592643da022f
Benak <matthew_benak@orfbenammacx10b.gallup.com>
2018-08-30 13:38:36 -0500

    vi mode for fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,5 @@
 eval (pipenv --completion)
-eval (gdircolors -c .config/dircolors/dircolors.256dark)
+eval (gdircolors -c $HOME/.config/dircolors/dircolors.ansi-dark)
+set -g theme_color_scheme solarized-dark
+set -g VIRTUAL_ENV_DISABLE_PROMPT 1
+set -x GARCTL_REGION us-east-1
--- fish/functions/ls.fish
+++
@@ -1,8 +0,0 @@
-function ls
-    switch (uname)
-    case Darwin
-        gls --color=auto $argv
-    case Linux
-        ls --color=auto $argv
-    end
-end
