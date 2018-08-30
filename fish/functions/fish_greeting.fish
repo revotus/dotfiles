3cc1886f61448a10d9ffef42da237465f70e24bc
Benak <matthew_benak@orfbenammacx10b.gallup.com>
2018-08-30 15:48:11 -0500

    bob stuff no links

--- fish/functions/fish_greeting.fish
+++ fish/functions/fish_greeting.fish
@@ -1,1 +1,6 @@
-/Users/matthew_benak/.config/fisherman/bobthefish/fish_greeting.fish
+function fish_greeting -d "What's up, fish?"
+  set_color $fish_color_autosuggestion
+  uname -nmsr
+  uptime
+  set_color normal
+end
