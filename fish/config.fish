d729ce98593e687672baac9f2e02a4739036b478
revotus <matt.benak@gmail.com>
2018-12-14 14:22:34 -0600

    redo

--- fish/config.fish
+++ fish/config.fish
@@ -1,5 +1,10 @@
 eval (pipenv --completion)
-eval (gdircolors -c $HOME/.config/dircolors/dircolors.ansi-dark)
+switch (uname)
+    case Linux
+        eval (dircolors -c $HOME/.config/dircolors/dircolors.ansi-dark)
+    case Darwin
+        eval (gdircolors -c $HOME/.config/dircolors/dircolors.ansi-dark)
+end
 set -g theme_color_scheme solarized-dark
 set -g VIRTUAL_ENV_DISABLE_PROMPT 1
 set -x GARCTL_REGION us-east-1
