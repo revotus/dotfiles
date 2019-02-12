22a6876c8fbc9a7527f4fb42217a351b0d2ad275
revotus <matt.benak@gmail.com>
2019-02-12 10:22:47 -0600

    dont write bytecode

--- fish/config.fish
+++ fish/config.fish
@@ -1,10 +1,16 @@
-eval (pipenv --completion)
+if which pipenv
+    eval (pipenv --completion)
+end
+
 switch (uname)
     case Linux
         eval (dircolors -c $HOME/.config/dircolors/dircolors.ansi-dark)
     case Darwin
         eval (gdircolors -c $HOME/.config/dircolors/dircolors.ansi-dark)
 end
+
 set -g theme_color_scheme solarized-dark
 set -g VIRTUAL_ENV_DISABLE_PROMPT 1
+
 set -x GARCTL_REGION us-east-1
+set -x PYTHONDONTWRITEBYTECODE 
